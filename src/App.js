7ae81ad3b9b41655668bd6088d177384a4f5653a
mariawenrose <mariarose.wen@gmail.com>
2019-11-12 23:26:56 +1300

    resized and stacked

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className = "orb-wrapper">
-          <div className = "blue-orb pulse"></div>
-          <div className = "purple-orb pulse"></div>
-          <div className = "orange-orb pulse"></div>
-          <div className = "yellow-orb pulse"></div>
-          <div className = "indigo-orb pulse"></div>
-          <div className = "green-orb pulse"></div>
-          <div className = "red-orb pulse"></div>
-        </div>
+          <div className="blue-orb pulse"></div>
+          <div className="purple-orb pulse"></div>
+          <div className="orange-orb pulse"></div>
+        <div className="indigo-orb pulse"></div>
+        <div className="yellow-orb pulse"></div>
+          <div className="green-orb pulse"></div>
+          <div className="red-orb pulse"></div>
       </header>
     </div>
   );
